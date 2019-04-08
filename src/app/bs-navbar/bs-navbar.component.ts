62b180865a33855ddb5986adc5cb7552a418d90f
Dhanush4444 <dhanushdazz@gmail.com>
2019-04-08 12:26:23 +0530

    Added route guard

---
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bs-navbar',
+  template: `
+    <p>
+      bs-navbar works!
+    </p>
+  `,
+  styles: []
+})
+export class BsNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
